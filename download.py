9200067ec2242e6c3be276e7245f240fa146fa16
Daniele Guido <gui.daniele@gmail.com>
2022-05-02 14:29:57 +0200

    better error reporting

--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import io
 import os
+import json
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
@@ -14,7 +15,8 @@ SCOPES = os.environ.get('GOOGLE_DRIVE_SCOPES', '').split(',')
 KEY_FILEPATH = os.environ.get('GOOGLE_SPREADHSEEET_SERVICE_ACCOUNT_KEY', '')
 INITIAL_PATH = os.environ.get('GOOGLE_DRIVE_INITIAL_PATH', '')
 DOWNLOADABLE_MIMETYPES = os.environ.get(
-    'DOWNLOADABLE_MIMETYPES', 'image/jpg,application/pdf').split(',')
+    'DOWNLOADABLE_MIMETYPES',
+    'image/jpg,application/pdf,image/tiff').split(',')
 
 print(
     'Welcome to Drive Fever!\n\n',
@@ -40,19 +42,23 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(
 service = build('drive', 'v3', credentials=credentials)
 
 
-def get_items_in_folder(folder_id, pageToken=None):
+def get_items_in_folder(folder_id, pageToken=None, offset=0):
     results = service.files().list(
         pageSize=10,
         pageToken=pageToken,
         fields="nextPageToken, files(id, name, mimeType)",
         q=f"'{folder_id}' in parents"
     ).execute()
-    print('get_items_in_folder', results)
     items = results.get('files', [])
+    print(
+        f'get_items_in_folder id:{folder_id} offset:{offset}'
+        f'\n- items n.:', len(items))
+    offset = offset + len(items)
     if results.get('nextPageToken', None) is not None:
         next_items = get_items_in_folder(
             folder_id=folder_id,
-            pageToken=results.get('nextPageToken'))
+            pageToken=results.get('nextPageToken'),
+            offset=offset)
         return items + next_items
     return items
 
@@ -68,28 +74,45 @@ def download_file(file_id, file_path, skip_if_exists=True):
     done = False
     while done is False:
         status, done = downloader.next_chunk()
-        print(f"Download {int(status.progress() * 100)}")
+        print(f"Download {int(status.progress() * 100)}%")
 
 
 def download_items(items=[], path='./data'):
     os.makedirs(path, exist_ok=True)
+    failed_items = []
+    done_items = []
     for item in items:
         file_path = os.path.join(
             path, sanitize_filename(item.get('name')))
         file_id = item.get('id')
-        print(file_path, file_id, item)
+        print(file_path, file_id, item.get('mimeType', ''))
         if item.get('mimeType', None) == FOLDER:
             print('create folder:', file_path)
             os.makedirs(file_path, exist_ok=True)
             folder_items = get_items_in_folder(item.get('id'))
-            download_items(items=folder_items, path=file_path)
+            done, failed = download_items(items=folder_items, path=file_path)
+            failed_items = failed_items + failed
+            done_items = done_items + done
         elif item.get('mimeType', None) in DOWNLOADABLE_MIMETYPES:
             download_file(
                 file_id=file_id, file_path=file_path, skip_if_exists=True)
+            done_items.append({
+                'file_path': file_path,
+                'file_id': file_id,
+                'mimeType': item.get('mimeType', None)
+            })
         else:
             print('could not download item', item)
+            failed_items.append({
+                'file_path': file_path,
+                'file_id': file_id,
+                'mimeType': item.get('mimeType', None)
+            })
+    return done_items, failed_items
 
 
 if __name__ == '__main__':
     items = get_items_in_folder(INITIAL_PATH)
-    download_items(items=items)
+    done_items, failed_items = download_items(items=items)
+    print(f'Done: {len(done_items)}, failed:{len(failed_items)}')
+    print(json.dumps(failed_items, indent=4))
