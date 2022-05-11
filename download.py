305f251fbf3a9e047517de6e77d73cf92ede3575
Daniele Guido <gui.daniele@gmail.com>
2022-05-11 10:25:17 +0200

    fix logs

--- download.py
+++ download.py
@@ -47,11 +47,10 @@ pysondb = db.getDb(PYSONDB_FILEPATH)
 
 
 def get_items_in_folder(folder_id, pageToken=None, offset=0):
-    print(f'get_items_in_folder id:{folder_id} offset:{offset} ...')
     results = service.files().list(
         pageSize=10,
         pageToken=pageToken,
-        fields="nextPageToken, files(id, name, mimeType)",
+        fields="nextPageToken, files(id, name, mimeType, modifiedTime)",
         q=f"'{folder_id}' in parents"
     ).execute()
     items = results.get('files', [])
@@ -59,6 +58,7 @@ def get_items_in_folder(folder_id, pageToken=None, offset=0):
         f'get_items_in_folder id:{folder_id} offset:{offset}'
         f'\n- items n.:', len(items))
     offset = offset + len(items)
+
     if results.get('nextPageToken', None) is not None:
         next_items = get_items_in_folder(
             folder_id=folder_id,
@@ -70,6 +70,7 @@ def get_items_in_folder(folder_id, pageToken=None, offset=0):
 
 def download_file(file_id, file_path, skip_if_exists=True):
     print(f'\ndownload_file {file_id} to {file_path}')
+
     if skip_if_exists and os.path.exists(file_path):
         print('...skipping, file exists already and skip_if_exists=True')
         return
@@ -86,13 +87,30 @@ def download_items(items=[], path='./data'):
     os.makedirs(path, exist_ok=True)
     failed_items = []
     done_items = []
-    for item in items:
+    for i, item in enumerate(items):
+        file_id = item.get('id')
+        skip_if_exists = False
+        records = pysondb.getBy({'gid': file_id})
         file_path = os.path.join(
             path, sanitize_filename(item.get('name')))
-        file_id = item.get('id')
-        print(file_path, file_id, item.get('mimeType', ''))
+        file_mimetype = item.get('mimeType', 'unknown')
+        print(
+            f'\n{i + 1} of {len(items)}'
+            f'\n- gid: {file_id}'
+            f'\n- file_path: {file_path}'
+            f'\n- mimetype: {file_mimetype}'
+        )
+        if not records:
+            pysondb.add({
+                'gid': file_id,
+                'modifiedTime': item.get('modifiedTime')
+            })
+        elif records[0].get('modifiedTime') == item.get('modifiedTime'):
+            print('- status: NOT MODIFIED, skip if file exists.')
+            skip_if_exists = True
+
         if item.get('mimeType', None) == FOLDER:
-            print('create folder:', file_path)
+            print('- create folder:', file_path)
             os.makedirs(file_path, exist_ok=True)
             folder_items = get_items_in_folder(item.get('id'))
             done, failed = download_items(items=folder_items, path=file_path)
@@ -100,14 +118,15 @@ def download_items(items=[], path='./data'):
             done_items = done_items + done
         elif item.get('mimeType', None) in DOWNLOADABLE_MIMETYPES:
             download_file(
-                file_id=file_id, file_path=file_path, skip_if_exists=True)
+                file_id=file_id, file_path=file_path,
+                skip_if_exists=skip_if_exists)
             done_items.append({
                 'file_path': file_path,
                 'file_id': file_id,
                 'mimeType': item.get('mimeType', None)
             })
         else:
-            print('could not download item', item)
+            print(f'WARNING: {file_mimetype} not in DOWNLOADABLE_MIMETYPES')
             failed_items.append({
                 'file_path': file_path,
                 'file_id': file_id,
@@ -117,9 +136,9 @@ def download_items(items=[], path='./data'):
 
 
 if __name__ == '__main__':
-    print('---')
-    print(f'Start on: {INITIAL_PATH}')
+    print(f'\nStart on: {INITIAL_PATH}\n---')
     items = get_items_in_folder(INITIAL_PATH)
+    print(f'\nDownload n.items: {len(items)}\n---')
     done_items, failed_items = download_items(items=items)
     print(f'Done: {len(done_items)}, failed:{len(failed_items)}')
     print(json.dumps(failed_items, indent=4))
